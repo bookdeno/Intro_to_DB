42a1cf070275d2b152c54000630550392a4e37a2
Tradeworld <bookdeno@example.com>
2025-12-14 03:30:14 -0800

    Fix Order_Details table to include quantity DOUBLE

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,44 +3,38 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 CREATE TABLE Authors (
-    author_id INT NOT NULL AUTO_INCREMENT,
-    author_name VARCHAR(215) NOT NULL,
-    PRIMARY KEY (author_id)
-) ENGINE=INNODB;
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
 
 CREATE TABLE Books (
-    book_id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(130) NOT NULL,
-    author_id INT NOT NULL,
-    price DOUBLE NOT NULL,
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
     publication_date DATE,
-    PRIMARY KEY (book_id),
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
-) ENGINE=INNODB;
+);
 
 CREATE TABLE Customers (
-    customer_id INT NOT NULL AUTO_INCREMENT,
-    customer_name VARCHAR(215) NOT NULL,
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
     email VARCHAR(215),
-    address TEXT,
-    PRIMARY KEY (customer_id)
-) ENGINE=INNODB;
+    address TEXT
+);
 
 CREATE TABLE Orders (
-    order_id INT NOT NULL AUTO_INCREMENT,
-    customer_id INT NOT NULL,
+    order_id INT PRIMARY KEY,
+    customer_id INT,
     order_date DATE,
-    PRIMARY KEY (order_id),
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-) ENGINE=INNODB;
+);
 
 CREATE TABLE Order_Details (
-    orderdetailid INT NOT NULL AUTO_INCREMENT,
-    order_id INT NOT NULL,
-    book_id INT NOT NULL,
-    quantity INT NOT NULL,
-    PRIMARY KEY (orderdetailid),
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
-) ENGINE=INNODB;
-
+);
